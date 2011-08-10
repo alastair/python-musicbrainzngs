f47a8760e7c38e2080f73873f2bf9998fe677ee4
Alastair Porter <alastair@porter.net.nz>
2011-08-09 22:55:28 -0400

    tests

---
+++ test/test_mbxml.py
@@ -0,0 +1,14 @@
+import unittest
+import os
+import sys
+sys.path.append(os.path.abspath(".."))
+import mbxml
+
+class MbXML(unittest.TestCase):
+    
+    def testMakeBarcode(self):
+        expected = ('<ns0:metadata xmlns:ns0="http://musicbrainz.org/ns/mmd-2.0#">'
+                    '<ns0:release-list><ns0:release ns0:id="trid"><ns0:barcode>12345</ns0:barcode>'
+                    '</ns0:release></ns0:release-list></ns0:metadata>')
+        xml = mbxml.make_barcode_request({'trid':'12345'})
+        self.assertEqual(expected, xml)
