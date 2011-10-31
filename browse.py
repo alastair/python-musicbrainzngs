5b9332d52e8b090d407b83d08e7e7ac622b31c62
Hartmut Goebel <h.goebel@goebel-consult.de>
2011-10-31 16:45:38 +0100

    Added sample/test-script for browse_*().

---
+++ browse.py
@@ -0,0 +1,17 @@
+
+from pprint import pprint
+import musicbrainz as m
+
+RELEASE = '10269c7f-e208-478e-b688-4ca6472cfcf2'
+LABEL = '47e718e1-7ee4-460c-b1cc-1192a841c6e5'
+
+def main():
+    pprint(m.browse_release(label=LABEL, limit=3, release_status='official'))
+    pprint(m.browse_artist(release=RELEASE, limit=3))
+    pprint(m.browse_label(release=RELEASE, limit=3))
+    pprint(m.browse_recording(release=RELEASE, limit=3))
+    pprint(m.browse_release_group(release=RELEASE, limit=3,
+                                  release_type='album'))
+
+if __name__ == "__main__":
+    main()
