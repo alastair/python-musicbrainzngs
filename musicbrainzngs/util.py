4903def7b062273e433dc4f7806795e952a7f33f
Wieland Hoffmann <themineo@gmail.com>
2012-04-03 23:12:39 +0200

    Make sure util._unicode works on python3
    str objects in python3 do not have a decode() method
    
    Signed-off-by: Wieland Hoffmann <themineo@gmail.com>

--- musicbrainzngs/util.py
+++ musicbrainzngs/util.py
@@ -6,18 +6,22 @@
 import sys
 import locale
 
+from . import compat
+
 def _unicode(string, encoding=None):
     """Try to decode byte strings to unicode.
     This can only be a guess, but this might be better than failing.
     It is safe to use this on numbers or strings that are already unicode.
     """
-    if isinstance(string, str):
+    if isinstance(string, compat.unicode):
+        unicode_string = string
+    elif isinstance(string, compat.bytes):
         # use given encoding, stdin, preferred until something != None is found
         if encoding is None:
             encoding = sys.stdin.encoding
         if encoding is None:
             encoding = locale.getpreferredencoding()
-        unicode_string = unicode(string, encoding, "ignore")
+        unicode_string = string.decode(encoding, "ignore")
     else:
-        unicode_string = unicode(string)
+        unicode_string = string.decode()
     return unicode_string.replace('\x00', '').strip()
