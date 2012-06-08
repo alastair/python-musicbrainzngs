be528732ebe9a9cb6071f7a1112dd6f885b063e4
Alastair Porter <alastair@porter.net.nz>
2012-06-08 13:15:41 -0400

    Parse track length element
    Fixes #73
    Reported by Patrick Speiser <doskir@gmail.com>
    Signed-off-by: Alastair Porter <alastair@porter.net.nz>

--- musicbrainzngs/mbxml.py
+++ musicbrainzngs/mbxml.py
@@ -435,7 +435,7 @@ def parse_track_list(tl):
 
 def parse_track(track):
 	result = {}
-	elements = ["number", "position", "title"]
+	elements = ["number", "position", "title", "length"]
 	inner_els = {"recording": parse_recording}
 
 	result.update(parse_elements(elements, track))
