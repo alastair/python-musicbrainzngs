19cd960a84bb6e0d5c31d93b860f882bb286b9ac
Wieland Hoffmann <themineo@gmail.com>
2012-07-18 15:50:47 +0200

    Add tests for release group types
    Signed-off-by: Wieland Hoffmann <themineo@gmail.com>

---
+++ test/test_mbxml_release_group.py
@@ -0,0 +1,32 @@
+# Tests for parsing of release queries
+
+import unittest
+import os
+import sys
+# Insert .. at the beginning of path so we use this version instead
+# of something that's already been installed
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import musicbrainzngs
+from musicbrainzngs import mbxml
+from test import _common
+
+class GetReleaseGroupTest(unittest.TestCase):
+    def setUp(self):
+        self.datadir = os.path.join(os.path.dirname(__file__), "data",
+                "release-group")
+
+    def testTypesExist(self):
+        fn = os.path.join(self.datadir,
+                          "f52bc6a1-c848-49e6-85de-f8f53459a624.xml")
+        res = mbxml.parse_message(open(fn))["release-group"]
+        self.assertTrue("type" in res)
+        self.assertTrue("primary-type" in res)
+        self.assertTrue("secondary-type-list" in res)
+
+    def testTypesResult(self):
+        fn = os.path.join(self.datadir,
+                          "f52bc6a1-c848-49e6-85de-f8f53459a624.xml")
+        res = mbxml.parse_message(open(fn))["release-group"]
+        self.assertEqual("Soundtrack", res["type"])
+        self.assertTrue("Album", res["primary-type"])
+        self.assertTrue("Soundtrack", res["secondary-type-list"])
