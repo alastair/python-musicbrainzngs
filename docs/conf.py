dc0d950c656de60f8702bf4a6f939bb77d9f13f9
Johannes Dewender <github@JonnyJD.net>
2013-01-29 23:32:24 +0100

    doc: force default style for MB colors
    Signed-off-by: Johannes Dewender <github@JonnyJD.net>

--- docs/conf.py
+++ docs/conf.py
@@ -95,6 +95,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
+# force default theme on readthedocs
+html_style = "/default.css"
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -137,7 +140,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
