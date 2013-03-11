0cbe2001c56453683eea3634831ba3ee17405cb4
Alastair Porter <alastair@porter.net.nz>
2013-03-11 21:47:09 +0100

    0.3 release!
    Signed-off-by: Alastair Porter <alastair@porter.net.nz>

--- musicbrainzngs/musicbrainz.py
+++ musicbrainzngs/musicbrainz.py
@@ -15,7 +15,7 @@ from musicbrainzngs import mbxml
 from musicbrainzngs import util
 from musicbrainzngs import compat
 
-_version = "0.3dev"
+_version = "0.3"
 _log = logging.getLogger("musicbrainzngs")
 
 
