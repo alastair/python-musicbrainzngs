alastair/python-musicbrainzngs

commit 291025c25d8903848bb01d90e36d1fd8689ceaf7

submission: move list conversion to api functions
Converting single items to lists is user input transformation and this
should be done directly in the API functions (or at least be visible
from there).

While removing this conversion in the backend,
I also changed the names of a couple of variables so one can guess the
number of entities envolved.
recording2isrcs gives multiple isrcs for each recording.
release2barcode gives only one barcode for every release.
Just "puids" doesn't tell much about what the variable holds.

The docstrings were also updated so they all use the same syntax for the
hint on what kind of dict they expect.
I documented that isrcs and co. can be give as lists or single values.

I also renamed variables with only 1 letter.
"p" is not a good variable name for a puid.

We don't have to test if a rating is "int".
str(rating) will make it an str, no matter if it was int, str
or unicode before. (these shouldn't have encoding problems)

Again, I change the indentation to 4 spaces in the functions involved.

Signed-off-by: Johannes Dewender <github@JonnyJD.net>

musicbrainzngs/musicbrainz.py
@@ -815,51 +815,65 @@ def get_releases_in_collection(collection):
 
 # Submission methods
 
-def submit_barcodes(barcodes):
-	"""Submits a set of {release1: barcode1, release2:barcode2}
+def submit_barcodes(release2barcode):
+    """Submits a set of {release_id1: barcode, ...}
 
-	Must call auth(user, pass) first"""
-	query = mbxml.make_barcode_request(barcodes)
-	return _do_mb_post("release", query)
+    Must call auth(user, pass) first"""
+    query = mbxml.make_barcode_request(release2barcode)
+    return _do_mb_post("release", query)
 
-def submit_puids(puids):
+def submit_puids(recording2puids):
     """Submit PUIDs.
+    Submits a set of {recording_id1: [puid1, ...], ...}
+    or {recording_id1: puid, ...}.
 
     Must call auth(user, pass) first"""
-    query = mbxml.make_puid_request(puids)
+    rec2puids = dict()
+    for (rec, puids) in recording2puids.items():
+        rec2puids[rec] = puids if isinstance(rec, list) else [puids]
+    query = mbxml.make_puid_request(rec2puids)
     return _do_mb_post("recording", query)
 
-def submit_echoprints(echoprints):
+def submit_echoprints(recording2echoprints):
     """Submit echoprints.
+    Submits a set of {recording_id1: [echoprint1, ...], ...}
+    or {recording_id1: echoprint, ...}.
 
     Must call auth(user, pass) first"""
-    query = mbxml.make_echoprint_request(echoprints)
+    rec2echos = dict()
+    for (rec, echos) in recording2echoprints.items():
+        rec2echos[rec] = echos if isinstance(rec, list) else [echos]
+    query = mbxml.make_echoprint_request(rec2echos)
     return _do_mb_post("recording", query)
 
-def submit_isrcs(recordings_isrcs):
+def submit_isrcs(recording2isrcs):
     """Submit ISRCs.
-    Submits a set of {recording-id: [isrc1, isrc2, ...]}
+    Submits a set of {recording-id1: [isrc1, ...], ...}
+    or {recording_id1: isrc, ...}.
 
     Must call auth(user, pass) first"""
-    query = mbxml.make_isrc_request(recordings_isrcs=recordings_isrcs)
+    rec2isrcs = dict()
+    for (rec, isrcs) in recording2isrcs.items():
+        rec2isrcs[rec] = isrcs if isinstance(rec, list) else [isrcs]
+    query = mbxml.make_isrc_request(rec2isrcs)
     return _do_mb_post("recording", query)
 
-def submit_tags(artist_tags={}, recording_tags={}):
+def submit_tags(artist2tags={}, recording2tags={}):
     """Submit user tags.
     Artist or recording parameters are of the form:
-    {'entityid': [taglist]}
+    {entity_id1: [tag1, ...], ...}
 
     Must call auth(user, pass) first"""
-    query = mbxml.make_tag_request(artist_tags, recording_tags)
+    query = mbxml.make_tag_request(artist2tags, recording2tags)
     return _do_mb_post("tag", query)
 
-def submit_ratings(artist_ratings={}, recording_ratings={}):
+def submit_ratings(artist2rating={}, recording2rating={}):
     """ Submit user ratings.
     Artist or recording parameters are of the form:
-    {'entityid': rating}
+    {entity_id1: rating, ...}
 
     Must call auth(user, pass) first"""
-    query = mbxml.make_rating_request(artist_ratings, recording_ratings)
+    query = mbxml.make_rating_request(artist2rating, recording2rating)
     return _do_mb_post("rating", query)
 
 def add_releases_to_collection(collection, releases=[]):

musicbrainzngs/mbxml.py
@@ -507,106 +507,99 @@ def parse_alias(alias):
     return result
 
 ###
-def make_barcode_request(barcodes):
-	NS = "http://musicbrainz.org/ns/mmd-2.0#"
-	root = ET.Element("{%s}metadata" % NS)
-	rel_list = ET.SubElement(root, "{%s}release-list" % NS)
-	for release, barcode in barcodes.items():
-		rel_xml = ET.SubElement(rel_list, "{%s}release" % NS)
-		bar_xml = ET.SubElement(rel_xml, "{%s}barcode" % NS)
-		rel_xml.set("{%s}id" % NS, release)
-		bar_xml.text = barcode
+def make_barcode_request(release2barcode):
+    NS = "http://musicbrainz.org/ns/mmd-2.0#"
+    root = ET.Element("{%s}metadata" % NS)
+    rel_list = ET.SubElement(root, "{%s}release-list" % NS)
+    for release, barcode in release2barcode.items():
+        rel_xml = ET.SubElement(rel_list, "{%s}release" % NS)
+        bar_xml = ET.SubElement(rel_xml, "{%s}barcode" % NS)
+        rel_xml.set("{%s}id" % NS, release)
+        bar_xml.text = barcode
 
-	return ET.tostring(root, "utf-8")
+    return ET.tostring(root, "utf-8")
 
-def make_puid_request(puids):
-	NS = "http://musicbrainz.org/ns/mmd-2.0#"
-	root = ET.Element("{%s}metadata" % NS)
-	rec_list = ET.SubElement(root, "{%s}recording-list" % NS)
-	for recording, puid_list in puids.items():
-		rec_xml = ET.SubElement(rec_list, "{%s}recording" % NS)
-		rec_xml.set("id", recording)
-		p_list_xml = ET.SubElement(rec_xml, "{%s}puid-list" % NS)
-		l = puid_list if isinstance(puid_list, list) else [puid_list]
-		for p in l:
-			p_xml = ET.SubElement(p_list_xml, "{%s}puid" % NS)
-			p_xml.set("id", p)
-
-	return ET.tostring(root, "utf-8")
-
-def make_echoprint_request(echoprints):
-	NS = "http://musicbrainz.org/ns/mmd-2.0#"
-	root = ET.Element("{%s}metadata" % NS)
-	rec_list = ET.SubElement(root, "{%s}recording-list" % NS)
-	for recording, echoprint_list in echoprints.items():
-		rec_xml = ET.SubElement(rec_list, "{%s}recording" % NS)
-		rec_xml.set("id", recording)
-		e_list_xml = ET.SubElement(rec_xml, "{%s}echoprint-list" % NS)
-		l = echoprint_list if isinstance(echoprint_list, list) else [echoprint_list]
-		for e in l:
-			e_xml = ET.SubElement(e_list_xml, "{%s}echoprint" % NS)
-			e_xml.set("id", e)
-
-	return ET.tostring(root, "utf-8")
-
-def make_tag_request(artist_tags, recording_tags):
-	NS = "http://musicbrainz.org/ns/mmd-2.0#"
-	root = ET.Element("{%s}metadata" % NS)
-	rec_list = ET.SubElement(root, "{%s}recording-list" % NS)
-	for rec, tags in recording_tags.items():
-		rec_xml = ET.SubElement(rec_list, "{%s}recording" % NS)
-		rec_xml.set("{%s}id" % NS, rec)
-		taglist = ET.SubElement(rec_xml, "{%s}user-tag-list" % NS)
-		for t in tags:
-			usertag_xml = ET.SubElement(taglist, "{%s}user-tag" % NS)
-			name_xml = ET.SubElement(usertag_xml, "{%s}name" % NS)
-			name_xml.text = t
-	art_list = ET.SubElement(root, "{%s}artist-list" % NS)
-	for art, tags in artist_tags.items():
-		art_xml = ET.SubElement(art_list, "{%s}artist" % NS)
-		art_xml.set("{%s}id" % NS, art)
-		taglist = ET.SubElement(art_xml, "{%s}user-tag-list" % NS)
-		for t in tags:
-			usertag_xml = ET.SubElement(taglist, "{%s}user-tag" % NS)
-			name_xml = ET.SubElement(usertag_xml, "{%s}name" % NS)
-			name_xml.text = t
-
-	return ET.tostring(root, "utf-8")
-
-def make_rating_request(artist_ratings, recording_ratings):
-	NS = "http://musicbrainz.org/ns/mmd-2.0#"
-	root = ET.Element("{%s}metadata" % NS)
-	rec_list = ET.SubElement(root, "{%s}recording-list" % NS)
-	for rec, rating in recording_ratings.items():
-		rec_xml = ET.SubElement(rec_list, "{%s}recording" % NS)
-		rec_xml.set("{%s}id" % NS, rec)
-		rating_xml = ET.SubElement(rec_xml, "{%s}user-rating" % NS)
-		if isinstance(rating, int):
-			rating = "%d" % rating
-		rating_xml.text = rating
-	art_list = ET.SubElement(root, "{%s}artist-list" % NS)
-	for art, rating in artist_ratings.items():
-		art_xml = ET.SubElement(art_list, "{%s}artist" % NS)
-		art_xml.set("{%s}id" % NS, art)
-		rating_xml = ET.SubElement(art_xml, "{%s}user-rating" % NS)
-		if isinstance(rating, int):
-			rating = "%d" % rating
-		rating_xml.text = rating
-
-	return ET.tostring(root, "utf-8")
-
-def make_isrc_request(recordings_isrcs):
+def make_puid_request(recording2puids):
     NS = "http://musicbrainz.org/ns/mmd-2.0#"
     root = ET.Element("{%s}metadata" % NS)
     rec_list = ET.SubElement(root, "{%s}recording-list" % NS)
-    for rec, isrcs in recordings_isrcs.items():
+    for recording, puid_list in recording2puids.items():
+        rec_xml = ET.SubElement(rec_list, "{%s}recording" % NS)
+        rec_xml.set("id", recording)
+        puid_list_xml = ET.SubElement(rec_xml, "{%s}puid-list" % NS)
+        for puid in puid_list:
+            puid_xml = ET.SubElement(puid_list_xml, "{%s}puid" % NS)
+            puid_xml.set("id", puid)
+
+    return ET.tostring(root, "utf-8")
+
+def make_echoprint_request(recording2echoprints):
+    NS = "http://musicbrainz.org/ns/mmd-2.0#"
+    root = ET.Element("{%s}metadata" % NS)
+    rec_list = ET.SubElement(root, "{%s}recording-list" % NS)
+    for recording, echoprint_list in recording2echoprints.items():
+        rec_xml = ET.SubElement(rec_list, "{%s}recording" % NS)
+        rec_xml.set("id", recording)
+        echoprint_list_xml = ET.SubElement(rec_xml, "{%s}echoprint-list" % NS)
+        for echoprint in echoprint_list:
+            echo_xml = ET.SubElement(echoprint_list_xml, "{%s}echoprint" % NS)
+            echo_xml.set("id", echoprint)
+
+    return ET.tostring(root, "utf-8")
+
+def make_tag_request(artist2tags, recording2tags):
+    NS = "http://musicbrainz.org/ns/mmd-2.0#"
+    root = ET.Element("{%s}metadata" % NS)
+    rec_list = ET.SubElement(root, "{%s}recording-list" % NS)
+    for rec, tags in recording2tags.items():
+        rec_xml = ET.SubElement(rec_list, "{%s}recording" % NS)
+        rec_xml.set("{%s}id" % NS, rec)
+        taglist = ET.SubElement(rec_xml, "{%s}user-tag-list" % NS)
+        for tag in tags:
+            usertag_xml = ET.SubElement(taglist, "{%s}user-tag" % NS)
+            name_xml = ET.SubElement(usertag_xml, "{%s}name" % NS)
+            name_xml.text = tag
+    art_list = ET.SubElement(root, "{%s}artist-list" % NS)
+    for art, tags in artist2tags.items():
+        art_xml = ET.SubElement(art_list, "{%s}artist" % NS)
+        art_xml.set("{%s}id" % NS, art)
+        taglist = ET.SubElement(art_xml, "{%s}user-tag-list" % NS)
+        for tag in tags:
+            usertag_xml = ET.SubElement(taglist, "{%s}user-tag" % NS)
+            name_xml = ET.SubElement(usertag_xml, "{%s}name" % NS)
+            name_xml.text = tag
+
+    return ET.tostring(root, "utf-8")
+
+def make_rating_request(artist2rating, recording2rating):
+    NS = "http://musicbrainz.org/ns/mmd-2.0#"
+    root = ET.Element("{%s}metadata" % NS)
+    rec_list = ET.SubElement(root, "{%s}recording-list" % NS)
+    for rec, rating in recording2rating.items():
+        rec_xml = ET.SubElement(rec_list, "{%s}recording" % NS)
+        rec_xml.set("{%s}id" % NS, rec)
+        rating_xml = ET.SubElement(rec_xml, "{%s}user-rating" % NS)
+        rating_xml.text = str(rating)
+    art_list = ET.SubElement(root, "{%s}artist-list" % NS)
+    for art, rating in artist2rating.items():
+        art_xml = ET.SubElement(art_list, "{%s}artist" % NS)
+        art_xml.set("{%s}id" % NS, art)
+        rating_xml = ET.SubElement(art_xml, "{%s}user-rating" % NS)
+        rating_xml.text = str(rating)
+
+    return ET.tostring(root, "utf-8")
+
+def make_isrc_request(recording2isrcs):
+    NS = "http://musicbrainz.org/ns/mmd-2.0#"
+    root = ET.Element("{%s}metadata" % NS)
+    rec_list = ET.SubElement(root, "{%s}recording-list" % NS)
+    for rec, isrcs in recording2isrcs.items():
         if len(isrcs) > 0:
             rec_xml = ET.SubElement(rec_list, "{%s}recording" % NS)
             rec_xml.set("{%s}id" % NS, rec)
-            isrc_list = isrcs if isinstance(isrcs, list) else [isrcs]
             isrc_list_xml = ET.SubElement(rec_xml, "{%s}isrc-list" % NS)
-            isrc_list_xml.set("{%s}count" % NS, str(len(isrc_list)))
-            for isrc in isrc_list:
+            isrc_list_xml.set("{%s}count" % NS, str(len(isrcs)))
+            for isrc in isrcs:
                 isrc_xml = ET.SubElement(isrc_list_xml, "{%s}isrc" % NS)
                 isrc_xml.set("{%s}id" % NS, isrc)
     return ET.tostring(root, "utf-8")

commit 22c96155f0f03b5aeddcfc819cedab27fd394ca5

fix list submission for isrcs, puids and echoprints
A bug was introduced with
291025c25d8903848bb01d90e36d1fd8689ceaf7 (in version 0.4):
Isrcs were always enclosed in [], even if they were already given as a
list.
So in 0.3 only giving isrcs as lists works
and in 0.4 only giving isrcs as single isrc works.

Now both should actually work.

Signed-off-by: Johannes Dewender <github@JonnyJD.net>

musicbrainzngs/musicbrainz.py
@@ -974,7 +974,7 @@ def submit_puids(recording_puids):
     """
     rec2puids = dict()
     for (rec, puids) in recording_puids.items():
-        rec2puids[rec] = puids if isinstance(rec, list) else [puids]
+        rec2puids[rec] = puids if isinstance(puids, list) else [puids]
     query = mbxml.make_puid_request(rec2puids)
     return _do_mb_post("recording", query)
 
@@ -985,7 +985,7 @@ def submit_echoprints(recording_echoprints):
     """
     rec2echos = dict()
     for (rec, echos) in recording_echoprints.items():
-        rec2echos[rec] = echos if isinstance(rec, list) else [echos]
+        rec2echos[rec] = echos if isinstance(echos, list) else [echos]
     query = mbxml.make_echoprint_request(rec2echos)
     return _do_mb_post("recording", query)
 
@@ -996,7 +996,7 @@ def submit_isrcs(recording_isrcs):
     """
     rec2isrcs = dict()
     for (rec, isrcs) in recording_isrcs.items():
-        rec2isrcs[rec] = isrcs if isinstance(rec, list) else [isrcs]
+        rec2isrcs[rec] = isrcs if isinstance(isrcs, list) else [isrcs]
     query = mbxml.make_isrc_request(rec2isrcs)
     return _do_mb_post("recording", query)
 