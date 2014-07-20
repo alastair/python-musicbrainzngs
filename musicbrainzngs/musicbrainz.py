a7004af715699d0acd234a7e043f89003b9f5a80
Johannes Dewender <github@JonnyJD.net>
2014-07-20 13:10:53 +0200

    use the same regex for lucene escapes, close #143
    The escapes for the value were updated recently but the escapes for the
    query were forgotten.
    Now we use a variable for the regex to reduce duplication.
    
    Signed-off-by: Johannes Dewender <github@JonnyJD.net>

--- musicbrainzngs/musicbrainz.py
+++ musicbrainzngs/musicbrainz.py
@@ -677,12 +677,13 @@ def _do_mb_search(entity, query='', fields={},
 	for the given entity type.
 	"""
 	# Encode the query terms as a Lucene query string.
+	lucene_special = r'([+\-&|!(){}\[\]\^"~*?:\\\/])'
 	query_parts = []
 	if query:
 		clean_query = util._unicode(query)
 		if fields:
-			clean_query = re.sub(r'([+\-&|!(){}\[\]\^"~*?:\\])',
-					r'\\\1', clean_query)
+			clean_query = re.sub(lucene_special, r'\\\1',
+					     clean_query)
 			if strict:
 				query_parts.append('"%s"' % clean_query)
 			else:
@@ -702,7 +703,7 @@ def _do_mb_search(entity, query='', fields={},
 
 		# Escape Lucene's special characters.
 		value = util._unicode(value)
-		value = re.sub(r'([+\-&|!(){}\[\]\^"~*?:\\\/])', r'\\\1', value)
+		value = re.sub(lucene_special, r'\\\1', value)
 		if value:
 			if strict:
 				query_parts.append('%s:"%s"' % (key, value))
