057659470a588be936c5a345aba063c2f9976e7a
Wieland Hoffmann <themineo@gmail.com>
2016-06-04 13:08:04 +0200

    Support only requests versions that requests-mock works with
    Test only the lowest one and the most recent one - let's hope there are
    no breaking changes in between (there were none at the time of this
    commit).
    
    Signed-off-by: Wieland Hoffmann <themineo@gmail.com>

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     packages=['musicbrainzngs'],
     cmdclass={'test': test },
     install_requires=[
-        'requests>=1.2.1'
+        'requests>=2.1.0'
     ],
     tests_require=[
         'requests-mock'
