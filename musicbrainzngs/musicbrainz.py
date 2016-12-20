2cc2266f8b2102556cffa4fd75b036ed2d8e9d61
Shen-Ta Hsieh <ibmibmibm.tw@gmail.com>
2016-12-20 18:19:34 +0800

    add "work-level-rels" support for recording lookup
    musicbrainz support "work-level-rels" for recording since 2011-10-10
    Reference: https://tickets.metabrainz.org/browse/MBS-3223
    
    Signed-off-by: Shen-Ta Hsieh <ibmibmibm.tw@gmail.com>

--- musicbrainzngs/musicbrainz.py
+++ musicbrainzngs/musicbrainz.py
@@ -54,7 +54,7 @@ VALID_INCLUDES = {
     'recording': [
         "artists", "releases", # Subqueries
         "discids", "media", "artist-credits", "isrcs",
-        "annotation", "aliases"
+        "work-level-rels", "annotation", "aliases"
     ] + TAG_INCLUDES + RATING_INCLUDES + RELATION_INCLUDES,
     'release': [
         "artists", "labels", "recordings", "release-groups", "media",
