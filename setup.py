e4bca39720c7c0a1de38b1663faa3475cf5b7eff
Alastair Porter <alastair@porter.net.nz>
2020-01-11 18:37:44 +0100

    Release 0.7.1
    Add long_description to setup.py for pipy
    
    Signed-off-by: Alastair Porter <alastair@porter.net.nz>

--- setup.py
+++ setup.py
@@ -4,14 +4,19 @@ from setuptools import setup
 
 from musicbrainzngs import musicbrainz
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="musicbrainzngs",
     version=musicbrainz._version,
     description="Python bindings for the MusicBrainz NGS and"
     " the Cover Art Archive webservices",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     author="Alastair Porter",
     author_email="alastair@porter.net.nz",
-    url="https://python-musicbrainzngs.readthedocs.org/",
+    url="https://python-musicbrainzngs.readthedocs.io/",
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     packages=['musicbrainzngs'],
     license='BSD 2-clause',
