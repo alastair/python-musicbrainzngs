98490724951dc034c2ff1b61ccc872a039874b82
Allen Wittenauer <aw@effectivemachines.com>
2023-07-23 09:55:50 -0700

    Parse first-release-date in Recordings (#7)
    Co-authored-by: Alastair Porter <alastair@porter.net.nz>

--- test/test_mbxml_recording.py
+++ test/test_mbxml_recording.py
@@ -19,13 +19,23 @@ class GetRecordingTest(unittest.TestCase):
 
         self.assertEqual(4, len(rels))
         # Original attributes
-        attributes = rels[2]["attribute-list"]
+        print(rels[0])
+        print(rels[1])
+        print(rels[2])
+        print(rels[3])
+        attributes = rels[0]["attribute-list"]
         self.assertEqual("piano", attributes[0])
 
         # New attribute dict format
-        attributes = rels[2]["attributes"]
-        expected = {"attribute": "piano", "credited-as": "Yamaha and Steinway pianos"}
+        attributes = rels[0]["attributes"]
+        expected = {"attribute": "piano",
+                    "credited-as": "Yamaha and Steinway pianos",
+                    "type-id": "b3eac5f9-7859-4416-ac39-7154e2e8d348"}
         self.assertEqual(expected, attributes[0])
+
+    def testFirstReleaseDate(self):
+        res = _common.open_and_parse_test_data(self.datadir, "f606f733-c1eb-43f3-93c1-71994ea611e3-artist-rels.xml")
+        self.assertEqual(res["recording"]["first-release-date"], "1984")
 
     def testTagsAndGenres(self):
         res = _common.open_and_parse_test_data(self.datadir, "58169b2c-e31a-4a46-8741-71c672a089ac-tags-genres.xml")
